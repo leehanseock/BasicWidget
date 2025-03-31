b2a9d067bf2d577af6e3ada2c66b1e9b4d470391
leehanseock <leehanseock@gmail.com>
2025-03-31 16:03:49 +0900

    버튼을 클릭했을 때 동작하는 Java 코드 작성

--- app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/basicwidget/MainActivity.java
@@ -2,6 +2,9 @@ package kr.ac.kopo.basicwidget;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,24 @@ public class MainActivity extends AppCompatActivity {
         text3.setText("Hurry up, tomorrow Hurry up, tomorrow Hurry up, tomorrow Hurry up, tomorrow" +
                 "Hurry up, tomorrow Hurry up, tomorrow Hurry up, tomorrow");
         text3.setSingleLine(true);
+
+        edit1 = findViewById(R.id.edit1);
+        textResult = findViewById(R.id.textResult);
+        Button btnResult = findViewById(R.id.btnResult);
+
+//        btnResult.setOnClickListener(v -> {
+//            String school = edit1.getText().toString();
+//            textResult.setText("결과: " + school);
+//        });
+        btnResult.setOnClickListener(btnListener);
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String school = edit1.getText().toString();
+            String result = school+"에 합격하셨습니다.🎇";
+            textResult.setText(result);
+            textResult.setTextColor(Color.rgb(128,65,217));
+        }
+    };
 }
